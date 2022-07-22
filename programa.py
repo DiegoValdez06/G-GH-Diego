53c22e02c8cfa5184c690d541f75d278dc73b2a1
DiegoValdez06 <diegopinal478@gmail.com>
2022-07-22 02:24:57 +0000

    Se modifica para sumer 3 numeros al archivo programa.py

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
-n1=float(input("Introduce el primer numero: "))
-n2=float(input("Introduce el segundo numero: "))
-suma=n1+n2
-
-print("La suma es: ",suma)
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 =int(input("Ingrese el tercer numero: "))
+suma = num1+num2+num3
+print("la suma de los numero es: ", suma)
